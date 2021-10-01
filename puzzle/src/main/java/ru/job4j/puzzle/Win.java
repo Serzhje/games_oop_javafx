cb9303eeaeed72a58793b3a378c69ba8061012aa
Serzhje <7prite@gmail.com>
2021-10-01 21:30:15 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -10,16 +10,16 @@ public class Win {
         return false;
     }
 
-    public static boolean horizonLine (int[][] board, int index) {
+    public static boolean horizonLine(int[][] board, int index) {
         for (int i = 0; i < board[index].length; i++) {
-            if(board[index][i] != 1) {
+            if (board[index][i] != 1) {
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean verticalLine (int[][] board, int index) {
+    public static boolean verticalLine(int[][] board, int index) {
         for (int i = 0; i < board.length; i++) {
             if (board[i][index] != 1) {
                return false;
